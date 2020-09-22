61f33d73241f3704e5297383ae2823c750948289
bfalsetto <benjamin.falsetto@ocsbstudent.ca>
2020-09-22 14:28:54 +0000

    created Einstein.java

---
+++ Einstein.java
@@ -0,0 +1,15 @@
+/**
+* This application calculates the amount of energy that could be released.
+* from an object
+*
+* @author  Ben Falsetto
+* @version 1.0
+* @since   2020-09-22
+*/
+ 
+public class Einstein {
+  public static void main(String[] args) {
+    
+  }
+  
+}
