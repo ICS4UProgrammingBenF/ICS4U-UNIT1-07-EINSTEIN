9f25d51fdddf290175c2fc55a601d52b38654eb1
bfalsetto <benjamin.falsetto@ocsbstudent.ca>
2020-09-23 19:44:15 +0000

    Style check passed, fixed random number generation bug, finished program

--- Einstein.java
+++ Einstein.java
@@ -1,7 +1,7 @@
 //importing the necessary classes
-import java.util.Scanner;
 import java.lang.Math;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
 * This application calculates the amount of energy that could be released.
@@ -15,7 +15,11 @@ import java.util.Random;
 public class Einstein {
   
   //generate a random number
-  public static int SentenceNum()  {
+  
+  /**
+   * The style checker tells me I need a javadoc here.
+  */
+  public static int sentenceNum()  {
     
     //declare min and max values
     int min = 1;
@@ -23,47 +27,46 @@ public class Einstein {
     int range = (max - min + 1);
     
     //choose a number to return
-    int phraseNum = (int)Math.random() * (max - min +1 ) + min;
+    //int phraseNum = (int)Math.random() * (max - min +1 ) + min;
     //int phraseNum = rand.nextInt(max);
+    int phraseNum = (int) (1 + (Math.random() * 5));
     
     //return the number generated to the function caller
     return phraseNum;
     
   }
   
-  //take random number, return a sentence
-  public static String SentenceChooser(int randPhraseNum) {
+  /**
+   * take random number, return a sentence.
+  */
+  public static String sentenceChooser(int randPhraseNum) {
     
     //declare variables
-    String randPhrase1 = "Message1";
-    String randPhrase2 = "Message2";
-    String randPhrase3 = "Message3";
-    String randPhrase4 = "Message4";
-    String randPhrase5 = "Message5";
+    String randPhrase1 = "I wouldn't do that if I were you...";
+    String randPhrase2 = "That's not such a... you know what? Go right ahead";
+    String randPhrase3 = "Why don't you go ahead, and I'll meet you after?";
+    String randPhrase4 = "Something tells me you're on some sort of watchlist now";
+    String randPhrase5 = "I'll watch from back here, thanks";
     String message = "";
     
     if (randPhraseNum == 1) {
       message = randPhrase1;
-    }
-    else if (randPhraseNum == 2) {
+    } else if (randPhraseNum == 2) {
       message = randPhrase2;
-    }
-    else if (randPhraseNum == 3) {
+    } else if (randPhraseNum == 3) {
       message = randPhrase3;
-    }
-    
-    else if (randPhraseNum == 4) {
+    } else if (randPhraseNum == 4) {
       message = randPhrase4;
-    }
-    
-    else if (randPhraseNum == 5) {
+    } else if (randPhraseNum == 5) {
       message = randPhrase5;
     }
-    
     
     return message;
   }
   
+  /**
+   * Main section of code.
+  */
   public static void main(String[] args) {
     
     //ask the user for mass of object
@@ -84,14 +87,15 @@ public class Einstein {
     kiloTon = energy / kiloTonConv;
     
     //get a random number for the phrase generator
-    int randPhraseNum = SentenceNum();
-    System.out.println("phrase number chosen: " + randPhraseNum);
+    int randPhraseNum = sentenceNum();
+    //System.out.println("phrase number chosen: " + randPhraseNum);
     
     //send the random number to the sentence chooser
-    String badIdea = SentenceChooser(randPhraseNum);
+    String badIdea = sentenceChooser(randPhraseNum);
     
     //display to the user the amount of energy possible
     System.out.println("If " + mass + "Kg was converted to energy, it would produce " + energy + "J of energy");
+    System.out.println("That's the equivalent of a " + kiloTon + " Kiloton bomb!");
     
     //tell the user it's a bad idea
     System.out.println(badIdea);
